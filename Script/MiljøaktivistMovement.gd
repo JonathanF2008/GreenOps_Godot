3363da795c78e0dbfab6954acc5356f156d6769b
JonathanF2008 <jofra013@osloskolen.no>
2025-11-20 11:16:06 +0100

    Gamemanager

--- Script/MiljøaktivistMovement.gd
+++ Script/MiljøaktivistMovement.gd
@@ -3,18 +3,26 @@ extends CharacterBody2D
 @onready var agent: NavigationAgent2D = $NavigationAgent2D
 
 var speed := 150.0
+@export var max_health := 100
+var health := max_health
+
+var game_manager: Node 
 
 func _ready():
 	agent.path_desired_distance = 4
 	agent.target_desired_distance = 4
-
+	if game_manager == null:
+		game_manager = get_tree().get_root().get_node("GameManager") 
 
 func _process(delta):
-	if Input.is_action_just_pressed("move_to_click"):
+	# Kun aktiv spiller kan motta input
+	if is_visible() and Input.is_action_just_pressed("move_to_click"):
 		agent.target_position = get_global_mouse_position()
 
-
 func _physics_process(delta):
+	if not is_visible():
+		return  
+
 	if agent.is_navigation_finished():
 		velocity = Vector2.ZERO
 		return
@@ -23,3 +31,15 @@ func _physics_process(delta):
 	var direction = (next_point - global_position).normalized()
 	velocity = direction * speed
 	move_and_slide()
+
+func take_damage(amount: int):
+	health -= amount
+	if health <= 0:
+		die()
+
+func die():
+	hide()
+	set_process(false)
+	set_physics_process(false)
+	if game_manager:
+		game_manager.on_player_death()
--- Script/gamemanager.gd
+++ Script/gamemanager.gd
@@ -1,15 +1,25 @@
 extends Node
 
-var active_player
-
-signal player_selected(player)
+@export var characters: Array[Node] = []
+var current_player_index := 0
 
 func _ready():
-	connect("player_selected", Callable(self, "_on_player_selected"))
+	activate_current_player()
 
-func _on_player_selected(player):
-	if active_player and active_player != player:
-		active_player.set_selected(false)
+func activate_current_player():
+	for character in characters:
+		character.hide()
+		character.set_process(false)
+		character.set_physics_process(false)
 
-	active_player = player
-	active_player.set_selected(true)
+	if current_player_index < characters.size():
+		var player = characters[current_player_index]
+		player.show()
+		player.set_process(true)
+		player.set_physics_process(true)
+	else:
+		print("Alle spillere er døde! Game over.")
+
+func on_player_death():
+	current_player_index += 1
+	activate_current_player()
