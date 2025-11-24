1997b19a3fddc3cc1bc72711a02090e97a2e1216
JonathanF2008 <jofra013@osloskolen.no>
2025-11-24 09:56:50 +0100

    .

--- Script/MiljøaktivistMovement.gd
+++ Script/MiljøaktivistMovement.gd
@@ -4,27 +4,40 @@ extends CharacterBody2D
 
 var speed := 150.0
 @export var max_health := 100
-var health := max_health 
+var health := max_health
+
+var is_active := false
+
+func set_active(value: bool):
+	is_active = value
+	visible = value
+	set_process(value)
+	set_physics_process(value)
+
+func _process(delta):
+	if not is_active:
+		return
+	
+	if Input.is_action_just_pressed("move_to_click"):
+		agent.set_target_position(get_global_mouse_position())
 
 func _physics_process(delta):
-	if not is_visible():
-		return  
+	if not is_active:
+		return
 
 	if agent.is_navigation_finished():
 		velocity = Vector2.ZERO
-		return
+	else:
+		var next = agent.get_next_path_position()
+		var direction = (next - global_position).normalized()
+		velocity = direction * speed
 
-	var next_point = agent.get_next_path_position()
-	var direction = (next_point - global_position).normalized()
-	velocity = direction * speed
 	move_and_slide()
 
-func take_damage(amount: int):
+func take_damage(amount):
 	health -= amount
 	if health <= 0:
 		die()
-
+	
 func die():
-	hide()
-	set_process(false)
-	set_physics_process(false)
+	queue_free()
