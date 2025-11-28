66d2977924083d00686fda341cc18b2c499a3e78
Benjamin-LucasM <bemya001@osloskolen.no>
2025-11-28 08:23:30 +0100

    ferdig
    flyttet på npc så de ikke skyter i vegg og la til esc som exit key

--- tomenu.gd
+++ tomenu.gd
@@ -1,6 +1,5 @@
 extends Node
 
 func _input(event):
-	if event is InputEventKey and event.pressed:
-		if event.shift_pressed and event.keycode == KEY_1:
-			get_tree().change_scene_to_file("res://start.tscn")
+	if event.is_action_pressed("ui_cancel"):
+		get_tree().change_scene_to_file("res://start.tscn")
